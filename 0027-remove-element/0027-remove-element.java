831fece84d41ddbf1352ce80e3be2e953496cf5e
Kelly <101918739+miuponn@users.noreply.github.com>
2025-04-26 01:16:17 -0400

    Update 0027-remove-element.java

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++){        // iterate through the array
             if (nums[i] != val){                      // if current element is not equal to val
                 nums[k] = nums[i];                    // overwrite element at index k with current element
-                k++;                                  // increment count of valid elementw
+                k++;                                  // increment count of valid elements
             } 
         }
         return k;                                     // return number of elements not equal to val
