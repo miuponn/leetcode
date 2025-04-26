088b72cb084eab8bee9e8d9e98c0cc75a1938937
Kelly <101918739+miuponn@users.noreply.github.com>
2025-04-26 01:15:26 -0400

    Time: 0 ms (100%), Space: 41.9 MB (82.99%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,13 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0;                                    // initialize counter k = elements that are not val
+        int k = 0;                                    // counter for number of elements not equal to val
 
-        for (int i = 0; i < nums.length; i++){        // iterate over nums array
-            if (nums[i] != val){                     // if nums[i] != val
-                nums[k] = nums[i];                    // override k index in nums with new val
-                k++;                                  // increment k
+        for (int i = 0; i < nums.length; i++){        // iterate through the array
+            if (nums[i] != val){                      // if current element is not equal to val
+                nums[k] = nums[i];                    // overwrite element at index k with current element
+                k++;                                  // increment count of valid elementw
             } 
         }
-        return k;
+        return k;                                     // return number of elements not equal to val
     }
 }
