038ae61ab83683b04a34d09dfbf0bc00b66a4f25
Kelly <101918739+miuponn@users.noreply.github.com>
2025-04-26 01:12:47 -0400

    Time: 0 ms (100%), Space: 42.2 MB (77.81%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,23 +1,23 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int last = m + n - 1;                      // pointer to last empty element in nums1
+        int last = m + n - 1;                      // pointer to last index of nums1 (merged array)
 
-        while (m > 0 && n > 0) {                   // iterate nums1 and nums2 starting from end of list 
-            if (nums1[m - 1] > nums2[n - 1]){      // if element of nums1 > element of nums2
-                nums1[last] = nums1[m - 1];        // assign element to position of pointer
-                m -= 1;                            // decrement nums1
-            } else {                               // if element of nums2 > element of nums1
-                nums1[last] = nums2[n - 1];        // assign element to position of pointer
-                n -= 1;                            // decrement nums2
+        while (m > 0 && n > 0) {                   // compare elements from the end of nums1 and nums2
+            if (nums1[m - 1] > nums2[n - 1]){      // place the larger element at the current last position
+                nums1[last] = nums1[m - 1];        
+                m--;                           
+            } else {                               
+                nums1[last] = nums2[n - 1];        
+                n--;                           
             }  
-            last -= 1;                             // decrement pointer
+            last --;                               // move last pointer to the left
         }
         
-        while (n > 0) {                            // if nums1 is done sorting but not nums2
-            nums1[last] = nums2[n - 1];            // fill rest of nums1 with remaining nums2
-            n -= 1;
-            last -= 1;
+        while (n > 0) {                            // place any remaining nums2 elements into nums1
+            nums1[last] = nums2[n - 1];            
+            n--;
+            last--;
         }
     }
 }
