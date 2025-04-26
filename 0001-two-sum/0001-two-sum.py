b97b6aa363a686c919348c294d55db6b97dd953e
Kelly <101918739+miuponn@users.noreply.github.com>
2025-04-26 01:17:40 -0400

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}                                   # initialize dict
 
-        for i, num in enumerate(nums):              # iterate over index, element in array nums
+        for i, num in enumerate(nums):              # iterate over index and element in array nums
             diff = target - num
             if diff in seen:                        # if difference exists in dict
                 return [seen[diff], i]              # return indices of difference, element
