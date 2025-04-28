aa839ebb24f30877e4b9146804143d25579baccd
Kelly <101918739+miuponn@users.noreply.github.com>
2025-04-28 02:14:43 -0400

    Time: 1 ms (63.75%), Space: 44.9 MB (60.81%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -2,8 +2,8 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         int l = 0;                              // slow pointer: tracks position of last unique element
 
-        for (int r = 0; r < nums.length; r++){  // fast pointer: scans each element
-            if (nums[r] != nums[l]) {           // if a new unique element is found, move slow pointer forward
+        for (int r = 1; r < nums.length; r++){  // fast pointer: scans each element
+            if (nums[r] != nums[r - 1]) {           // if a new unique element is found, move slow pointer forward
                 l++;
                 nums[l] = nums[r];              // overwrite with the new unique element
             }
