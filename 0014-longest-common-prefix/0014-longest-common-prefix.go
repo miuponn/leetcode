c66d4baad95d626bbc8c80eeb580105a9d23c318
Kelly Gao <kellygao@live.ca>
2026-02-25 23:43:09 -0500

    Time: 0 ms (100%), Space: 4.3 MB (72.78%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.go
@@ -0,0 +1,10 @@
+func longestCommonPrefix(strs []string) string {
+    for i := 0; i < len(strs[0]); i++ {
+        for j := 1; j < len(strs); j++ {
+            if i >= len(strs[j]) || strs[j][i] != strs[0][i] {
+                return strs[0][:i]
+            }
+        }
+    }
+    return strs[0]
+}
