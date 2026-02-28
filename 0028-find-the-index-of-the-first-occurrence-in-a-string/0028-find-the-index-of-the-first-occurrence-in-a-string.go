770c5557b0998df93f7c3e5972b29993b40e99e8
Kelly Gao <kellygao@live.ca>
2026-02-27 22:00:08 -0500

    Time: 0 ms (100%), Space: 4.2 MB (5.09%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
@@ -1,18 +1,37 @@
 func strStr(haystack string, needle string) int {
-    // NAIVE approach
-    // outer loop haystack o(n)
-    for i := 0; i < len(haystack); i++ {
-        // inner loop needle o(m)
-        for j := 0; j < len(needle); j++ {
-            // compare starting index i + character index j with needle character index j
-            if i + j >= len(haystack) || haystack[i+j] != needle[j] {
-                // if characters dont match or character out of bounds, break 
-                break // next i, j := 0 on break
-            }
-            if j == len(needle) - 1 { //if full needle found at i, return i
-                return i 
-            }
+    // KMP: LPS array 
+    if needle == "" { return 0 }
+    lps := make([]int, len(needle))                  // init LPS array
+    prevLPS, i := 0, 1                               // pointers for LPS len, array/prefix index
+    
+    // 1. build list of LPS for each prefix in needle
+    for i < len(needle){                             // for prefix in needle,
+        if needle[i] == needle[prevLPS] {            // 1. prefix forms new lps with prevLPS
+            lps[i] = prevLPS + 1                     //    - add new lps to list
+            i++                                      //    - increment next prefix
+            prevLPS++                                //    - increment prevLPS 
+        } else if prevLPS == 0 {                     // 2. no new lps, no prevLPS
+            lps[i] = 0                               //    - add 0 to list
+            i++                                      //    - increment next prefix
+        } else {                                     // 3. no new lps
+            prevLPS = lps[prevLPS - 1]               //    - decrement prevLPS to its prevLPS
+        }
+    }
+    // 2. search for needle in haystack
+    i, j := 0, 0                                    // pointers for haystack, needle
+    for i < len(haystack){                          // for char i as starting point 
+        if haystack[i] == needle[j]{                // 1. char i in haystack == char j in needle
+            i++                                     //     - increment char i, j 
+            j++
+        } else if j == 0 {                          // 2. mismatch, no chars j matched for i
+            i++                                     //     - check next char i as starting point
+        } else {                                    // 3. mismatch, chars j matched for i
+            j = lps[j - 1]                          //     - decrement char j to matched so far
+        }
+        if j == len(needle) {                       // full match found in haystack
+            return i - len(needle)                  //return starting index of needle in haystack
         }
     } 
-    return -1 //else return not found
-} // TC: n(n * m)
+    return -1
+} 
+
