fde72a6919d1e362cc9af27966c2a66905a64882
Kelly Gao <kellygao@live.ca>
2026-02-28 19:52:48 -0500

    Time: 4 ms (65.26%), Space: 8.3 MB (66.08%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.go
@@ -0,0 +1,32 @@
+func minEatingSpeed(piles []int, h int) int {
+    // range of possible k to binary search
+    lo, hi := 1, max(piles)
+    for lo <= hi {
+        // intial k = centre of range
+        k := (lo + hi) / 2 
+        hrs := 0
+        for _, pile := range piles {
+            hrs += ceil(pile, k)
+        }
+        if hrs > h {
+            lo = k + 1
+        } else if hrs <= h {
+            hi = k - 1
+        }
+    }
+    return lo
+}
+
+func ceil(a, b int) int {
+    return (a + b - 1) / b
+}
+
+func max(piles []int) int {
+    maximum := 0
+    for i := 0; i < len(piles); i++{
+        if piles[i] > maximum{
+            maximum = piles[i]
+        }
+    }
+    return maximum
+}
