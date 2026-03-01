f66fb9d0318a5b8f8908f7dacae9146ceca5da2f
Kelly Gao <kellygao@live.ca>
2026-02-28 20:30:43 -0500

    Time: 0 ms (100%), Space: 9.3 MB (5.99%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.go
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.go
@@ -1,13 +1,13 @@
 func minEatingSpeed(piles []int, h int) int {
     // range of possible k to binary search
     lo, hi := 1, max(piles)
-    for lo <= hi {
-        // intial k = centre of range
+    for lo <= hi {                                          // O(log n) binary search
+        // intial k = centre of range 
         k := (lo + hi) / 2 
         hrs := 0
-        for _, pile := range piles {
+        for _, pile := range piles {                
             hrs += ceil(pile, k)
-        }
+        }                                               
         if hrs > h {
             lo = k + 1
         } else if hrs <= h {
@@ -17,10 +17,11 @@ func minEatingSpeed(piles []int, h int) int {
     return lo
 }
 
+// helpers
 func ceil(a, b int) int {
     return (a + b - 1) / b
 }
-
+// o(n) max func
 func max(piles []int) int {
     maximum := 0
     for i := 0; i < len(piles); i++{
