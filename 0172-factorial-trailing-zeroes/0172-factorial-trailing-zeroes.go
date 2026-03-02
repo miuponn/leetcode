6f855a75b9d67f028a1efc714db42ad6e3277d02
Kelly Gao <kellygao@live.ca>
2026-03-01 20:37:22 -0500

    Time: 0 ms (100%), Space: 3.8 MB (100%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.go
@@ -0,0 +1,10 @@
+func trailingZeroes(n int) int {
+    k := 0                                  // k = num of trailing zeroes
+    for i := 1; i <= n; i++ {            // iterative log 5 n 
+        k += n/5                            // k = floor(n^i) until floor(n/5) = 5
+        n = n/5                             
+    }
+    return k
+}
+
+// O(log n) iterative factorial solution, can be O(n) if hardcode if statements until n > 10,000, but O(log n) works for n > 10^4
