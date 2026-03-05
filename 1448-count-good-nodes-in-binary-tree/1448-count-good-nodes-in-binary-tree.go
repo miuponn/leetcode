a9b86674757ab363a91d12a5074fd7655d88f219
Kelly Gao <kellygao@live.ca>
2026-03-05 16:10:20 -0500

    Time: 93 ms (17.49%), Space: 13.4 MB (36.55%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.go
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.go
@@ -7,19 +7,19 @@
  * }
  */
 func goodNodes(root *TreeNode) int {
-    if root == nil { return 0 }
+    if root == nil { return 0 }                     // nil check for base case
 
-    var dfs func(curr *TreeNode, maxVal int) int
+    var dfs func(curr *TreeNode, maxVal int) int        
         dfs = func(curr *TreeNode, maxVal int) int {
-        if curr == nil { return 0 }
-        count := 0
-        if curr.Val >= maxVal {
-            count = 1
-            maxVal = curr.Val
+        if curr == nil { return 0 }                 // nil check to exit early
+        count := 0                                  // var for result (num of good nodes)
+        if curr.Val >= maxVal {                     // process curr node = good node
+            count = 1                               // increment count
+            maxVal = curr.Val                       // update max for subtree
         }
-        count += dfs(curr.Left, maxVal)
-        count += dfs(curr.Right, maxVal)
-        return count
+        count += dfs(curr.Left, maxVal)             // recurse left sub, pass subtree max and return count
+        count += dfs(curr.Right, maxVal)            // recurse right sub, pass subtree max and return count
+        return count                                // base case: end of sub reached, return sub's count
     }
-    return dfs(root, root.Val)
+    return dfs(root, root.Val)                      // make call to dfs
 }
