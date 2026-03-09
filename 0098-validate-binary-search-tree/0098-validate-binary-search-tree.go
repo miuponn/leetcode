91ff03870be2deb067b9e3b89e3d4fd3f0a592cb
Kelly Gao <kellygao@live.ca>
2026-03-09 12:54:06 -0400

    Time: 0 ms (100%), Space: 7.1 MB (99.91%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.go
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.go
@@ -9,12 +9,14 @@
 func isValidBST(root *TreeNode) bool {
     if root == nil { return true }
 
-    var dfs func(curr *TreeNode, min int, max int) bool
+    var dfs func(curr *TreeNode, min int, max int) bool                     
         dfs = func(curr *TreeNode, min int, max int) bool {
             if curr == nil { return true }
-            if curr.Val <= min || curr.Val >= max { 
+            if curr.Val <= min || curr.Val >= max {
                 return false 
             }
+            // if left subtree, update max to curr node value. if right subtree, update min to curr node value.
+            // return true if left sub and right sub both true
             return dfs(curr.Left, min, curr.Val) && dfs(curr.Right, curr.Val, max)
         }
     return dfs(root, math.MinInt, math.MaxInt)
