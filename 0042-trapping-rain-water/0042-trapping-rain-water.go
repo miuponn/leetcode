b8ee472e560a44f0681c3d65c0d477cc107376cc
Kelly Gao <kellygao@live.ca>
2026-03-22 21:16:08 -0400

    Time: 0 ms (100%), Space: 8.1 MB (44.02%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.go
+++ 0042-trapping-rain-water/0042-trapping-rain-water.go
@@ -1,18 +1,18 @@
 func trap(height []int) int {
-    if len(height) == 0 { return 0 }
+    if len(height) == 0 { return 0 }                // empty list
 
-    l, r := 0, len(height) - 1
-    res, lMax, rMax := 0, height[l], height[r]
+    l, r := 0, len(height) - 1                      // left, right pointers
+    res, lMax, rMax := 0, height[l], height[r]      // left max, right max vars
 
     for l < r {
-        if lMax < rMax {
+        if lMax < rMax {                            // move left if left max < right max
             l++
-            lMax = max(height[l], lMax)
-            res += lMax - height[l]
-        } else {
-            r--
-            rMax = max(height[r], rMax)
-            res += rMax - height[r]
+            lMax = max(height[l], lMax)             // update left max if new max
+            res += lMax - height[l]                 // add new water in cusp to total 
+        } else {                                    // move right if right max <= left max
+            r--                         
+            rMax = max(height[r], rMax)             // update right max if new max
+            res += rMax - height[r]                 // add new water in cusp to total
         }
     }
     return res
