663d7b429a4d101735c01b94bc24989464dd08e5
Kelly Gao <kellygao@live.ca>
2026-03-30 19:24:33 -0400

    Time: 59 ms (73.95%), Space: 12.2 MB (72.27%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.go
+++ 0853-car-fleet/0853-car-fleet.go
@@ -1,33 +1,21 @@
 func carFleet(target int, position []int, speed []int) int {
-    cars := make([][2]int, len(position))
+    cars := make([][2]int, len(position))                        // init array of pairs (position, speed)
 
-    for i := 0; i < len(position); i++ {
+    for i := 0; i < len(position); i++ {                         // iterate and populate array of pairs 
         cars[i] = [2]int{position[i], speed[i]}
     }
-    sort.Slice(cars, func(i, j int) bool {
+    sort.Slice(cars, func(i, j int) bool {                       // sort array of pairs by position descending
         return cars[i][0] > cars[j][0]
     })
 
-    stack := []float64{}
-    for _, car := range cars {
-        arrival := float64(target - car[0]) / float64(car[1])
-        if len(stack) != 0 && arrival <= stack[len(stack)-1] {
-            continue
+    stack := []float64{}                                         // init stack for fleets (by arrival time)
+    for _, car := range cars {                                   // iterate over cars, starting from nearest
+        arrival := float64(target - car[0]) / float64(car[1])    // calculate arrival time of car
+        if len(stack) != 0 && arrival <= stack[len(stack)-1] {   // if arrival faster than prev car ahead, fleet forms
+            continue                                             // keep slower speed of prev car as fleet
         }
-        stack = append(stack, arrival)
+        stack = append(stack, arrival)                           // else, add arrival time as fleet to stack
     }
-    return len(stack)
+    return len(stack)                                            // return # of merged fleets
 }
-
-// initialize outer cars slice of inner arrays of size 2
-// iterate over position array, for each position, add position + speed to cars slice as inner array
-// sort cars slice by i > j for index 0 of inner array
-
-// initialize stack for arrival times
-// for each car in cars:
-// calculate arrival = target - position / speed
-// if stack not empty and arrival <= top element of stack:
-// push top element back onto stack
-// else, push arrival onto stack
-
-// at end of iteration loop, length of stack == result (return length of stack)
+// monotonic ascending stack, logn for sorting, n for iteration, time O(nlogn)
