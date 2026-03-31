252f6e287e5a30f81c04a1eb72a8a24f7a222468
Kelly Gao <kellygao@live.ca>
2026-03-31 16:35:48 -0400

    Time: 0 ms (100%), Space: 4.3 MB (88.67%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.go
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.go
@@ -1,12 +1,12 @@
 func findMin(nums []int) int {
     l, r := 0, len(nums) - 1
 
-    for l < r {
+    for l < r {                          // binary search to converge onto min val
         mid := (l + r) / 2
 
-        if nums[r] < nums[mid] {
+        if nums[r] < nums[mid] {         // min in later half, converge right
             l = mid + 1
-        } else if nums[r] > nums[mid] {
+        } else if nums[r] > nums[mid] {  // min in first half, converge left
             r = mid
         }
     }
