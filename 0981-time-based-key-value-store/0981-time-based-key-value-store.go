95650526cd2de510520d220261081cb05f3448c4
Kelly Gao <kellygao@live.ca>
2026-04-06 17:31:48 -0400

    Time: 43 ms (71.77%), Space: 60.9 MB (65.52%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.go
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.go
@@ -1,37 +1,37 @@
-type TimeMap struct {
+type TimeMap struct {                                                   // store map of key -> slice of (timestamp, value)
     m map[string][]pair
 }
 
-type pair struct {
+type pair struct {                                                      // helper struct pair for (timestamp, value)
     timestamp int
     value string
 }
 
-func Constructor() TimeMap {
+func Constructor() TimeMap {                                            // init m map with key -> slice of (timestamp, value)
     return TimeMap{m: make(map[string][]pair)}
 }
 
-func (this *TimeMap) Set(key string, value string, timestamp int)  {
+func (this *TimeMap) Set(key string, value string, timestamp int)  {    // add (timestamp, value) to key -> slice
     this.m[key] = append(this.m[key], pair{timestamp, value})
 }
 
-func (this *TimeMap) Get(key string, timestamp int) string {
-    pairs, ok := this.m[key]
+func (this *TimeMap) Get(key string, timestamp int) string {        
+    pairs, ok := this.m[key]                                            // return nil of key doesnt exist in map
     if !ok { return "" }
 
-    lo, hi := 0, len(pairs) - 1
-    for lo <= hi {
+    lo, hi := 0, len(pairs) - 1                                         
+    for lo <= hi {                                                      // binary search for target in map
         mid := (lo + hi)/2
         if pairs[mid].timestamp == timestamp {
-            return pairs[mid].value
+            return pairs[mid].value                                     // return value at target if target in map
         } else if pairs[mid].timestamp < timestamp {
             lo = mid + 1
         } else {
             hi = mid - 1
         }
     }
-    if hi < 0 { return "" } 
-    return pairs[hi].value
+    if hi < 0 { return "" }                                            // if target not in map and target < all timestamps, return nil
+    return pairs[hi].value                                             // if target not in map, return rightmost timestamp less than target
 }
 
 
