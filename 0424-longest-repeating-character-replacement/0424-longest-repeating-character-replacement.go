c6637118f0fa8784b95c4708960833079db6e350
Kelly Gao <kellygao@live.ca>
2026-04-07 10:14:54 -0400

    Time: 21 ms (26.97%), Space: 5.6 MB (17.68%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.go
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.go
@@ -1,17 +1,17 @@
 func characterReplacement(s string, k int) int {
-    m := make(map[byte]int)                 // map var to keep char -> count
-    l, maxFreq, res := 0, 0, 0
+    m := make(map[byte]int)                         // map var to keep char -> count
+    l, maxFreq, res := 0, 0, 0                              
 
-    for r := 0; r < len(s); r++ {
-        m[s[r]]++       // update count in letter
-        maxFreq = max(maxFreq, m[s[r]])
+    for r := 0; r < len(s); r++ {                   // increase window by next char in s
+        m[s[r]]++                                   // update count for char
+        maxFreq = max(maxFreq, m[s[r]])             // update count of most frequent char so far
 
-        for (r - l + 1) - maxFreq > k {
-            m[s[l]]--
-            l++
+        for (r - l + 1) - maxFreq > k {             // if chars to replace (window - freq char count) > replaceable
+            m[s[l]]--                               // decrement left char, shrink window
+            l++                                     
         }
 
-        res = max(res, (r - l + 1))
+        res = max(res, (r - l + 1))                 // update res with largest valid window so far
     }
     return res
 }
